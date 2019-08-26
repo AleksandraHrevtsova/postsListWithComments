d7736703eb4661f3454e6ddfbad7131d1456b6cc
AleksandraHrevtsova <47608805+AleksandraHrevtsova@users.noreply.github.com>
2019-08-26 21:57:12 +0300

    Updates

---
+++ precache-manifest.43487312eff8848eeb9df9e209fdb799.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39263aaf20e7e73983c665ca7bd881c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bd3210b6be7609edf6e",
+    "url": "/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "036b97daba20dc677c1b",
+    "url": "/static/js/2.bad52f5e.chunk.js"
+  },
+  {
+    "revision": "0bd3210b6be7609edf6e",
+    "url": "/static/js/main.c1032a21.chunk.js"
+  },
+  {
+    "revision": "89048e97ba64c09b953c",
+    "url": "/static/js/runtime~main.849bde4f.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
